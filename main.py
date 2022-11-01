68c6fd8d3ebf510150b9416cd595d9ba4e0501db
ANDRE LUIZ FRANCA BATISTA <01411769627>
2022-11-01 09:43:59 -0300

    Adding NN

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, render_template, request
 import os
+import predictions
 
 app = Flask(__name__, template_folder='templates')
 
@@ -11,7 +12,14 @@ def index():
 @app.route('/predict', methods=['POST', 'GET'])
 def predict():
     if request.method == 'POST':
-        return jsonify(request.form)
+        dados = [
+            float(request.form['comp_sepala']),
+            float(request.form['larg_sepala']),
+            float(request.form['comp_petala']),
+            float(request.form['larg_petala'])
+        ]
+        y_pred = predictions.predict(dados)
+        return jsonify({"resultado" : y_pred})
     elif request.method == 'GET':
         return jsonify({"mensagem" : "Utilize o formulário"})
 
---
+++ predictions.py
@@ -0,0 +1,35 @@
+import torch
+import numpy as np
+
+# Criar um modelo de Rede Neural
+class Net(torch.nn.Module):
+  def __init__(self, input_size, hidden_size, output_size):
+    super(Net, self).__init__()
+    self.input_size = input_size
+    self.hidden_size = hidden_size
+    self.output_size = output_size
+    self.fc1 = torch.nn.Linear(self.input_size, self.hidden_size) #full connected
+    self.relu = torch.nn.ReLU() #(0, infinito)
+    self.fc2 = torch.nn.Linear(self.hidden_size, self.output_size)
+    self.sigmoid = torch.nn.Sigmoid() #(0, 1)
+  def forward(self, x):
+    hidden = self.fc1(x)
+    relu = self.relu(hidden)
+    output = self.fc2(relu)
+    output = self.sigmoid(output)
+    return output
+
+def predict(dados):
+    #dados = [larg_sepala, comp_sepala, larg_petala, comp_petala]
+    entrada = torch.FloatTensor(dados)
+    # print(entrada)
+    # print(entrada.size())
+    input_size = entrada.size()[0]
+    hidden_size = 10
+    output_size = 3
+    modelo = Net(input_size, hidden_size, output_size)
+    modelo.load_state_dict(torch.load('./modelos/iris.dat'))
+    modelo.eval()
+    y_pred = modelo(entrada) # rodar o modelo com os dados de entrada 
+    y_pred = y_pred.detach().numpy()
+    return y_pred
