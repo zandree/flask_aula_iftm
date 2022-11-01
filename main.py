ddbb5f8b65cf2f56cb56ad7dcadde804172283be
ANDRE LUIZ FRANCA BATISTA <01411769627>
2022-11-01 09:20:06 -0300

    Adding route predict

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request
 import os
 
 app = Flask(__name__, template_folder='templates')
@@ -7,6 +7,15 @@ app = Flask(__name__, template_folder='templates')
 @app.route('/')
 def index():
     return render_template("index.html")
+
+@app.route('/predict', methods=['POST', 'GET'])
+def predict():
+    if request.method == 'POST':
+        return jsonify(request.form)
+    elif request.method == 'GET':
+        return jsonify({"mensagem" : "Utilize o formulário"})
+
+
 
 
 if __name__ == '__main__':
